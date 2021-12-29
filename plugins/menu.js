ad17b6b8a53a7905868e5b43b47a2790a786e0e0
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2021-12-29 21:34:46 +0700

    Create menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -20,8 +20,7 @@ const defaultMenu = {
 │
 ├ Uptime: *%uptime (%muptime)*
 ├ Database: %rtotalreg dari %totalreg
-├ Github:
-├ %github
+├ 
 └────
 %readmore`.trimStart(),
   header: '┌─〔 %category 〕',
